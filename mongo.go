e886f204f3c02f25db26e968f89b6752f59e4ad3
jackypan <87015598@qq.com>
2017-10-11 15:59:16 +0800

    init project

---
+++ mongo.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"gopkg.in/mgo.v2"
+	"fmt"
+)
+
+func NewMongoSession(host string, port int) *mgo.Session {
+	url := fmt.Sprintf("mongodb://%s:%d", host, port)
+	// Connect to our local mongo
+	session, err := mgo.Dial(url)
+	// Check if connection error, is mongo running?
+	if err != nil {
+		panic(err)
+	}
+	session.SetMode(mgo.Monotonic, true)
+	return session
+}
