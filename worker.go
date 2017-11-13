d8087075044cbda56aa1a380ea6b1488f49220f1
jackypan <87015598@qq.com>
2017-11-13 14:34:35 +0800

    add the error message

--- worker.go
+++ worker.go
@@ -40,7 +40,7 @@ func (w *Worker) Start() {
 			case job := <-w.JobChannel:
 				start := time.Now()
 				if err := job.Do(); err != nil {
-					log.Error(err)
+					log.WithFields(log.Fields{"component": "core", "error": err}).Error()
 				}
 				log.WithFields(log.Fields{"component": "core",}).Debugf("worker#%d spends %s", w.id, time.Now().Sub(start))
 			case <-w.quit:
