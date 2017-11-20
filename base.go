3692e0d5848c0439a781d9bcea77c8e113c0d030
jackypan <87015598@qq.com>
2017-11-20 21:00:55 +0800

    bug fix

--- base.go
+++ base.go
@@ -63,6 +63,6 @@ func (service *Service) FindByTimestamp(tab *mgo.Collection, start string, end s
 
 func (service *Service) All(tab *mgo.Collection, size int) (interface{}, error) {
 	var results []interface{}
-	err := tab.Find(bson.M{}).Sort("-timestamp").Limit(size).All(&results)
+	err := tab.Find(bson.M{}).Sort("-timestamp").Limit(size).All(results)
 	return results, err
 }
