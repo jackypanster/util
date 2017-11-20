80d702818369e0f56b1869b383b97e6d848caac3
jackypan <87015598@qq.com>
2017-11-20 21:06:19 +0800

    bug fix

--- base.go
+++ base.go
@@ -55,10 +55,12 @@ func (service *Service) Update(tab *mgo.Collection, id string, selector interfac
 	return tab.Update(selector, update)
 }
 
-func (service *Service) FindByTimestamp(tab *mgo.Collection, start string, end string, size int, result interface{}) error {
+func (service *Service) FindByTimestamp(tab *mgo.Collection, start string, end string, size int) (interface{}, error) {
 	from := ConvertTimestamp(start)
 	to := ConvertTimestamp(end)
-	return tab.Find(bson.M{"timestamp": bson.M{"$gte": from, "$lt": to}}).Sort("-timestamp").Limit(size).All(result)
+	var results []interface{}
+	err := tab.Find(bson.M{"timestamp": bson.M{"$gte": from, "$lt": to}}).Sort("-timestamp").Limit(size).All(&results)
+	return results, err
 }
 
 func (service *Service) All(tab *mgo.Collection, size int) (interface{}, error) {
