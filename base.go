4d8d712e2c02d4c8d0ed7d17d727e07bf4c3374c
jackypan <87015598@qq.com>
2017-11-21 15:44:10 +0800

    remove cache

--- base.go
+++ base.go
@@ -23,59 +23,31 @@ type Operator interface {
 	FindByTimestamp(string, string) (interface{}, error)
 }
 
-type Service struct {
-	cache map[string]interface{}
-}
+type Service struct{}
 
 func NewService() *Service {
-	return &Service{
-		cache: make(map[string]interface{}),
-	}
+	return &Service{}
 }
 
-func (service *Service) Insert(tab *mgo.Collection, id string, doc interface{}) (interface{}, error) {
-	err := tab.Insert(doc)
-	if err == nil {
-		service.cache[id] = doc
-	}
-	return service.cache[id], err
+func (service *Service) Insert(tab *mgo.Collection, id string, doc interface{}) error {
+	return tab.Insert(doc)
 }
 
 func (service *Service) Remove(tab *mgo.Collection, id string) error {
-	err := tab.Remove(bson.M{"id": id})
-	if err == nil {
-		delete(service.cache, id)
-	}
-	return err
+	return tab.Remove(bson.M{"id": id})
 }
 
 func (service *Service) RemoveAll(tab *mgo.Collection) error {
 	_, err := tab.RemoveAll(nil)
-	if err == nil {
-		service.cache = make(map[string]interface{})
-	}
 	return err
 }
 
-func (service *Service) Find(tab *mgo.Collection, id string) (interface{}, error) {
-	if _, ok := service.cache[id]; ok {
-		return service.cache[id], nil
-	}
-
-	var result interface{}
-	err := tab.Find(bson.M{"id": id}).One(&result)
-	if err == nil {
-		service.cache[id] = &result
-	}
-	return service.cache[id], err
+func (service *Service) Find(tab *mgo.Collection, id string, result interface{}) error {
+	return tab.Find(bson.M{"id": id}).One(result)
 }
 
-func (service *Service) Update(tab *mgo.Collection, id string, selector interface{}, update interface{}, doc interface{}) (interface{}, error) {
-	err := tab.Update(selector, update)
-	if err == nil {
-		service.cache[id] = doc
-	}
-	return service.cache[id], err
+func (service *Service) Update(tab *mgo.Collection, selector interface{}, update interface{}) error {
+	return tab.Update(selector, update)
 }
 
 func (service *Service) FindByDate(tab *mgo.Collection, date string, size int, results interface{}) error {
