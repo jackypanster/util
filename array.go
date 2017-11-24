0909b8d2abc969b1ec0640f3a62f1d399719621e
jackypan <87015598@qq.com>
2017-11-24 15:21:07 +0800

    remove empty string in array

--- array.go
+++ array.go
@@ -1,15 +1,17 @@
 package util
 
 func Uniq(src []string) []string {
-  m := make(map[string]int)
-  var dst []string
+	m := make(map[string]int)
+	var dst []string
 
-  for _, item := range src {
-    m[item] = 0
-  }
+	for _, item := range src {
+		m[item] = 0
+	}
 
-  for key := range m {
-    dst = append(dst, key)
-  }
-  return dst
+	for key := range m {
+		if len(key) > 0 {
+			dst = append(dst, key)
+		}
+	}
+	return dst
 }
