8c558384faff8e3307a6a63248b1d4f42995595e
zhibinpan <jackypanster@gmail.com>
2017-11-28 22:30:35 +0800

    check string whether it is empty or not

--- helper.go
+++ helper.go
@@ -2,7 +2,16 @@ package util
 
 import (
 	"log"
+	"strings"
 )
+
+func CheckStr(value string, name string) string {
+	str := strings.TrimSpace(value)
+	if len(str) == 0 {
+		log.Panicf("%s should not be empty", name)
+	}
+	return str
+}
 
 func CheckErr(err error) {
 	if err != nil {
