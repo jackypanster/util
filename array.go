028a94af038508082b469ef208aa3fbd53558f9a
jackypan <87015598@qq.com>
2017-11-28 15:04:39 +0800

    trim the space of string element

--- array.go
+++ array.go
@@ -1,17 +1,20 @@
 package util
 
+import "strings"
+
 func Uniq(src []string) []string {
 	m := make(map[string]int)
 	var dst []string
 
-	for _, item := range src {
-		m[item] = 0
+	for _, i := range src {
+		item := strings.TrimSpace(i)
+		if len(item) > 0 {
+			m[item] = 0
+		}
 	}
 
 	for key := range m {
-		if len(key) > 0 {
-			dst = append(dst, key)
-		}
+		dst = append(dst, key)
 	}
 	return dst
 }
