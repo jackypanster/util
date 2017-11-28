8db33407e47b2e960f277801b5d3e7e94f92d311
zhibinpan <jackypanster@gmail.com>
2017-11-28 23:03:34 +0800

    check empty array

--- helper.go
+++ helper.go
@@ -5,6 +5,27 @@ import (
 	"strings"
 )
 
+func CheckArray(values []string, name string) []string {
+	var results []string
+	if len(values) == 0 {
+		log.Panicf("%s should not be empty", name)
+	}
+	m := make(map[string]int)
+	for _, value := range values {
+		str := strings.TrimSpace(value)
+		if len(str) > 0 {
+			m[str] = 0
+		}
+	}
+	if len(m) == 0 {
+		log.Panicf("%s should not be empty", name)
+	}
+	for key := range m {
+		results = append(results, key)
+	}
+	return results
+}
+
 func CheckStr(value string, name string) string {
 	str := strings.TrimSpace(value)
 	if len(str) == 0 {
