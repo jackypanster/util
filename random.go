793e74156afdd48a8545df84922af9d6f576d4b8
jackypan <87015598@qq.com>
2017-12-01 13:21:08 +0800

    add random method for string

---
+++ random.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+  "time"
+  "math/rand"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandSeq(n int) string {
+  rand.Seed(time.Now().UnixNano())
+  b := make([]rune, n)
+  for i := range b {
+    b[i] = letters[rand.Intn(len(letters))]
+  }
+  return string(b)
+}
