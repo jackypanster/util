6a40ab891e4f36fe714b178323b615ee1f6ef92b
jackypan <87015598@qq.com>
2017-12-04 13:20:38 +0800

    update random int/string

--- random.go
+++ random.go
@@ -1,17 +1,21 @@
 package util
 
 import (
-  "time"
-  "math/rand"
+	"time"
+	"math/rand"
 )
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-func RandSeq(n int) string {
-  rand.Seed(time.Now().UnixNano())
-  b := make([]rune, n)
-  for i := range b {
-    b[i] = letters[rand.Intn(len(letters))]
-  }
-  return string(b)
+func RandomStr(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func RandomInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
