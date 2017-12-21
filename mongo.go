d7c75c3141e8445c318a096b214c8aa128cd8902
jackypan <87015598@qq.com>
2017-12-21 15:27:44 +0800

    add method 'NewMgoSession'

--- mongo.go
+++ mongo.go
@@ -6,7 +6,20 @@ import (
 )
 
 func NewMongoSession(host string, port int) *mgo.Session {
+	CheckStr(host, "host")
+
 	url := fmt.Sprintf("mongodb://%s:%d", host, port)
+	// Connect to our local mongo
+	session, err := mgo.Dial(url)
+	// Check if connection error, is mongo running?
+	CheckErr(err)
+
+	session.SetMode(mgo.Monotonic, true)
+	return session
+}
+
+func NewMgoSession(url string) *mgo.Session {
+	CheckStr(url, "url")
 	// Connect to our local mongo
 	session, err := mgo.Dial(url)
 	// Check if connection error, is mongo running?
