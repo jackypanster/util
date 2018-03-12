b624336b1f01a6bce0df2e0baece9dafcee6cabd
jackypan <87015598@qq.com>
2018-03-12 10:33:33 +0800

    update

--- log.go
+++ log.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"os"
+
 	log "github.com/Sirupsen/logrus"
 )
 
@@ -8,6 +10,13 @@ func init() {
 	formatter := new(log.JSONFormatter)
 	formatter.TimestampFormat = "02-01-2006 15:04:05"
 	log.SetFormatter(formatter)
+}
+
+func SetOutput(filename string) {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	CheckErrf(err, "fail to open file")
+	log.SetOutput(f)
+	defer f.Close()
 }
 
 func Warnf(fields map[string]interface{}, format string, args ...interface{}) {
