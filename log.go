d8c0d4567498c102989a4c8d94e81f2989bec641
jackypan <87015598@qq.com>
2018-03-12 10:20:35 +0800

    update

--- log.go
+++ log.go
@@ -4,6 +4,12 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+func init() {
+	formatter := new(log.JSONFormatter)
+	formatter.TimestampFormat = "02-01-2006 15:04:05"
+	log.SetFormatter(formatter)
+}
+
 func Warnf(fields map[string]interface{}, format string, args ...interface{}) {
 	if len(fields) != 0 {
 		log.WithFields(fields).Warnf(format, args)
