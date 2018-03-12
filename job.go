51db3bc799886903faecb24dcf97a68c0ca16773
jackypan <87015598@qq.com>
2018-03-12 20:26:44 +0800

    update

---
+++ job.go
@@ -0,0 +1,43 @@
+package util
+
+import "time"
+
+const (
+	HIGH = iota
+	NORMAL
+	LOW
+)
+
+type Task struct {
+	ID       string    `json:"id"`
+	Action   string    `json:"action"`
+	Priority int       `json:"priority"`
+	Time     time.Time `json:"time"`
+	Content  string    `json:"content"`
+}
+
+type TaskService struct {
+	*RedisService
+}
+
+func NewTaskService(redisService *RedisService) *TaskService {
+	CheckCondition(redisService == nil, "redisService should not be nil")
+	return &TaskService{
+		redisService,
+	}
+}
+
+func (self *TaskService) Enq(task *Task) error {
+	CheckCondition(task == nil, "task should not be nil")
+	return self.Rpush(task)
+}
+
+func (self *TaskService) Deq() (*Task, error) {
+	reply, err := self.Lpop()
+	if err != nil {
+		return nil, err
+	}
+	var task Task
+	ToInstance(reply, &task)
+	return &task, nil
+}
