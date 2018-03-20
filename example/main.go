8717c855c683a4955419fa2486f81debe7b83712
zhibinpan <jackypanster@gmail.com>
2018-03-20 23:55:29 +0800

    update

--- example/main.go
+++ example/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-	"os"
-
-	"github.com/jackypanster/util"
+	//"os"
+"time"
+//"fmt"
+"go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
 )
 
 func main() {
-	f, err := os.OpenFile("log/log.json", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-	util.CheckErrf(err, "fail to open file")
-	util.SetOutput(f)
-	util.SetDebug(true)
-	defer f.Close()
+	//f, err := os.OpenFile("log/log.json", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	
+	
+  cfg := zap.NewProductionConfig()
+  cfg.OutputPaths = append(cfg.OutputPaths, "./log/log.json")
+  cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+  logger, err := cfg.Build()
+  if err != nil {
+	  panic(err)
+  }
+  defer logger.Sync()
+  
+  //fmt.Printf("%#v\n", cfg)
+  const url = "http://example.com"
 
-	//util.Debugf(util.Map{"key": "value"}, "%s %d", "testing", 1)
-	//util.Infof(util.Map{"key": "value"}, "%s %s", "testing", "more")
-	//util.Warnf(util.Map{"key": "value"}, "%s", "testing")
-	//util.Errorf(util.Map{"key": "value"}, "%s", "testing")
-
-	//util.Errorf(util.Map{"key": "value"}, "")
-
-	//test_enq()
-	//test_deq()
-	test_trace()
+  // In most circumstances, use the SugaredLogger. It's 4-10x faster than most
+  // other structured logging packages and has a familiar, loosely-typed API.
+  sugar := logger.Sugar()
+  sugar.Infow("Failed to fetch URL.",
+	  // Structured context as loosely typed key-value pairs.
+	  "url", url,
+	  "attempt", 3,
+	  "backoff", time.Second,
+  )
+  sugar.Infof("Failed to fetch URL: %s", url)
 }
--- example/stack_trace.go
+++ example/stack_trace.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"errors"
+
 
 	"github.com/jackypanster/util"
 )
 
 func test_trace() {
-	err := errors.New("I don't know what to do yet")
-	if err != nil {
-		util.Panicf(util.Map{"key": "value", "cause": err}, "error occurs")
-	}
+
+		util.Infof(util.Map{"key": "value"}, "error occurs")
+	
 }
