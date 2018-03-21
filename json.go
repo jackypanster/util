5d3d416c60757c7b656eb81f7dd78ab52332f244
jackypan <87015598@qq.com>
2018-03-21 11:01:18 +0800

    update

--- json.go
+++ json.go
@@ -9,7 +9,6 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 func ToJsonString(v interface{}) (string, error) {
 	bs, err := json.Marshal(v)
 	if err != nil {
-		Errorf(Map{"error": err}, "unable to encode %#v", v)
 		return "", err
 	}
 	return string(bs), nil
@@ -19,7 +18,6 @@ func ToInstance(data string, v interface{}) error {
 	CheckStr(data, "data")
 	err := json.Unmarshal([]byte(data), v)
 	if err != nil {
-		Errorf(Map{"error": err}, "unable to decode %s", data)
 		return err
 	}
 	return nil
