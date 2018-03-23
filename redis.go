43bb9fb0ab0211f6d503e6e920f985a59f794c65
jackypan <87015598@qq.com>
2018-03-23 14:14:24 +0800

    update

--- redis.go
+++ redis.go
@@ -120,3 +120,43 @@ func GetRedisPool(url string) *redis.Pool {
 
 	return pool
 }
+
+func GetPool(url string, password string, db int) *redis.Pool {
+	CheckStr(url, "url")
+	pool := &redis.Pool{
+		MaxActive:   65536,
+		MaxIdle:     256,
+		IdleTimeout: 128 * time.Second,
+		Wait:        true,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", url)
+			if err != nil {
+				log.Panicf("fail to dial %s, error %s", url, err.Error())
+				return nil, err
+			}
+			if _, err := c.Do("AUTH", password); err != nil {
+				c.Close()
+				log.Panicf("fail to auth %s, error %s", password, err.Error())
+				return nil, err
+			}
+			if _, err := c.Do("SELECT", db); err != nil {
+				c.Close()
+				log.Panicf("fail to select db %d, error %s", db, err.Error())
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			if err != nil {
+				log.Panicf("fail to ping %s, error %s", url, err.Error())
+			}
+			return err
+		},
+	}
+
+	return pool
+}
