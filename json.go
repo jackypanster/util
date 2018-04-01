2b95940f374e89d3e4719a25bc4265713c50065b
jackypan <87015598@qq.com>
2018-04-01 14:53:28 +0800

    update

--- json.go
+++ json.go
@@ -15,7 +15,26 @@ func ToJsonString(v interface{}) (string, error) {
 	return string(bs), nil
 }
 
+func Encode(v interface{}) (string, error) {
+	CheckNil(v, "v")
+	bs, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
+}
+
 func ToInstance(str string, v interface{}) error {
+	CheckStr(str, "str")
+	CheckNil(v, "v")
+	err := json.Unmarshal([]byte(str), v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Decode(str string, v interface{}) error {
 	CheckStr(str, "str")
 	CheckNil(v, "v")
 	err := json.Unmarshal([]byte(str), v)
