6e4a6cb31ec9eb4da0344cf68da52c5bb24635df
jackypan <87015598@qq.com>
2018-05-20 09:33:10 +0800

    update

--- dispatcher.go
+++ dispatcher.go
@@ -32,14 +32,14 @@ func (d *Dispatcher) dispatch() {
 		select {
 		case job := <-JobQueue:
 			// a job request has been received
-			go func(job Job) {
-				// try to obtain a worker job channel that is available
-				// this will block until a worker is idle
-				jobChannel := <-d.workerPool
+			//go func(job Job) {
+			// try to obtain a worker job channel that is available
+			// this will block until a worker is idle
+			jobChannel := <-d.workerPool
 
-				// dispatch the job to the worker job channel
-				jobChannel <- job
-			}(job)
+			// dispatch the job to the worker job channel
+			jobChannel <- job
+			//}(job)
 		}
 	}
 }
